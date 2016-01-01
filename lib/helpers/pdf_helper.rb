9527c35137e890e64d6802f00716e4e45b334ce9
Candice Mccollough <candice.mccollough@careerbuilder.com>
2016-01-01 17:08:01 -0500

    implement bleed option

--- lib/helpers/pdf_helper.rb
+++ lib/helpers/pdf_helper.rb
@@ -5,12 +5,12 @@ module Cryptodating
     BUSINESS_CARD_300PPI = [X, Y]
 
     ## 45px on each side for standard bleed
-    #string, {}:sym
+    #string, {}:sym -> nil, side effects: creates file ./public/*.pdf
     def self.render_pdf(name, pdf_params={})
+      bleed = pdf_params[:bleed].nil? ? 0 : 90
+      Prawn::Document.generate "./public/#{name}.pdf", :page_size => [X + bleed, Y + bleed] do |pdf|
 
-      Prawn::Document.generate "./public/#{name}.pdf", :page_size => BUSINESS_CARD_300PPI do |pdf|
-
-        pdf.image "./public/imgs/pdfimgs/front.jpg", :at => [-25, Y-25]
+        pdf.image "./public/imgs/pdfimgs/front.jpg", :at => [-25 + ( bleed / 2), Y - 25 + ( bleed / 2 )]
 
         unless pdf_params[:name].nil?
           size = 100
@@ -23,7 +23,7 @@ module Cryptodating
           end
           pdf.font "./public/fonts/HelveticaNeue-UltraLight.ttf", :size => size do
             pdf.fill_color "878787"
-            pdf.draw_text "#{pdf_params[:name]}".upcase, :at => [55, Y-400]
+            pdf.draw_text "#{pdf_params[:name]}".upcase, :at => [55 + ( bleed / 2), Y - 400 + ( bleed / 2)]
           end
         end
 
@@ -31,7 +31,7 @@ module Cryptodating
         if pdf_params[:area_code]
           x_diff = 315
           pdf_params[:area_code].reverse_each() do |digit|
-            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff, Y-100]
+            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff + ( bleed / 2), Y-100 + ( bleed / 2)]
             x_diff += 150
           end
         end
@@ -39,7 +39,7 @@ module Cryptodating
         if pdf_params[:first_three]
           x_diff = 315
           pdf_params[:first_three].reverse_each() do |digit|
-            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff, Y-275]
+            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff + ( bleed / 2), Y-275 + ( bleed / 2)]
             x_diff+=150
           end
         end
@@ -47,13 +47,13 @@ module Cryptodating
         if pdf_params[:last_four]
           x_diff = 315
           pdf_params[:last_four].reverse_each() do |digit|
-            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff, Y-450]
+            pdf.image "./public/imgs/pdfimgs/#{digit}.jpg", :at => [X-x_diff + ( bleed / 2), Y-450 + ( bleed / 2)]
             x_diff+=150
           end
         end
         pdf.start_new_page
 
-        pdf.image "./public/imgs/pdfimgs/back-3.1.jpg", :at => [-25, Y-50]
+        pdf.image "./public/imgs/pdfimgs/back-3.1.jpg", :at => [-25 + ( bleed / 2), Y-40 + ( bleed / 2)]
       end
     end
 
